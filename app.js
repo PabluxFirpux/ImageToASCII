a5f4f5fbc57c74f5fb4bb25dfa261eab846b1051
PabluxFirpux <pfirpomolina@gmail.com>
2024-03-20 13:53:11 +0000

    recieving request in server

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express();
 const port = 23232
 app.use(express.json());
 // Routing
-app.send('/sendimg', async(req, res) => {
-    console.log('recieved request');
+app.post('/sendimg', async(req, res) => {
+    console.log('recieved request')
+    res.status(200).json({ message: "pedo"} )
 });
 
 
@@ -17,4 +18,4 @@ app.listen(port, () => console.log('API server is running on port ' + port));
 
 
 
-createAsciiHTML('./resources/mongus.webp');
+//createAsciiHTML('./resources/mongus.webp');
